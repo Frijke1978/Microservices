a0a8eac6b103eea27bb62e1399758b3825d00c32
frijke1978 <frijke1978@outlook.com>
2020-10-12 16:39:03 +0200

    Displaying the comments

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import CommentCreate from './CommentCreate';
 import axios from 'axios';
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList'
+
 
 export default () => {
     const [posts, setPosts] = useState({});
@@ -24,6 +26,7 @@ export default () => {
             >
                 <div className="card-body">
                     <h3>{post.title}</h3>
+                    <CommentList postId={post.id} />
                     <CommentCreate postId={post.id} />
                 </div>
             </div>
