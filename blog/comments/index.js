beff8be4e43468c5a26113cd3ad3d7d9930c29b4
frijke1978 <frijke1978@outlook.com>
2020-10-21 11:53:30 +0200

    Event syncing setup

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -10,39 +10,61 @@ app.use(cors());
 
 const commentsByPostId = {};
 
-
 app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] || []);
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', async (req, res) => {
-    const commentId = randomBytes(4).toString('hex');
-    const { content } = req.body;
-    const comments = commentsByPostId[req.params.id] || [];
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
 
-    comments.push({ id: commentId, content });
+  const comments = commentsByPostId[req.params.id] || [];
 
-    commentsByPostId[req.params.id] = comments;
+  comments.push({ id: commentId, content, status: 'pending' });
 
-    await axios.post('http://localhost:4005/events', {
-        type: 'CommentCreated',
-        data: {
-            id: commentId,
-            content,
-            postId: req.params.id
-        }
-    });
+  commentsByPostId[req.params.id] = comments;
 
-    res.status(201).send(comments);
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+      status: 'pending'
+    }
+  });
+
+  res.status(201).send(comments);
 });
 
+app.post('/events', async (req, res) => {
+  console.log('Event Received:', req.body.type);
 
-app.post('/events', (req, res) => {
-    console.log('Event Received', req.body.type);
+  const { type, data } = req.body;
 
-    res.send({});
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    });
+  }
+
+  res.send({});
 });
 
 app.listen(4001, () => {
-    console.log('Listening on port 4001');
+  console.log('Listening on 4001');
 });
